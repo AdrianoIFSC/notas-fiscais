a5e895958a54fa29765e6625bf5cd6e2b2ccb555
Adriano Canto Junior <140018392+AdrianoIFSC@users.noreply.github.com>
2023-08-18 10:10:10 -0300

    update

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import PySimpleGUI as sg
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
 sg.theme('Dark2')
 layout = [
     [sg.Text('CÓDIGO DO SIAFI:    '), sg.Input(key='usuario', size=(20, 1))],
     [sg.Text('DIGITE SUA SENHA: '), sg.Input(key='senha', password_char='*', size=(20, 1))],
     [sg.Text('NOME DA NOTA FISCAL COM EXTENÇÃO EM .PDF')],
-    [sg.Input(key='arquivo', size=(20,1))],
+    [sg.InputText(key='arquivo', size=(32,1))],
     [sg.Button('Começar')]
 ]
 janela = sg.Window('Automatização de Liquidação', layout)
@@ -15,100 +17,12 @@ while True:
     if eventos == sg.WIN_CLOSED:
         break
     if eventos == 'Começar':
-        usuario = valores['usuario']
-        senha = valores['senha']
         navegador = webdriver.Firefox()
         file = valores['arquivo']
-        navegador.get('http://localhost/php_project/menu.php')
-        arquivo_input = navegador.find_element('xpath', '/html/body/div/div[1]/div/form/div/input')
-        arquivo_input.send_keys(r'C:\Users\DAM\Documents\\' + file)
-        navegador.find_element('xpath', '/html/body/div/div[1]/div/form/input').click()
-        navegador.find_element('xpath', '//*[@id="resultado"]/div[1]/div[1]/button[1]').click()
+        navegador.get('*') # NO LUGAR DO # INSIRA A URL DO SITE QUE IRA RESUMIR A NOTA FISCAL
+        arquivo_input = navegador.find_element(By.XPATH, '*') # NO LUGAR DO * COLOQUE O XPATH DO CAMPO DE INPUT
+        arquivo_input.send_keys(r'*\\' + file) #NO LUGAR DO * COLOQUE O LOCAL DA PASTA ONDE ESTA LOCALIZADO AS NOTAS FISCAIS
+        navegador.find_element(By.XPATH, '*').click() # NO LUGAR DO * COLOQUE O XPATH DO BOTÃO DE EXTRAIR TEXTO
         navegador.execute_script("window.open('about:blank', '_blank');")
         navegador.switch_to.window(navegador.window_handles[1])
-        navegador.get('http://localhost/adriano.html')
-        navegador.find_element('xpath', '/html/body/form/div[1]/input').send_keys(valores['usuario'])
-        navegador.find_element('xpath', '/html/body/form/div[2]/input').send_keys(valores['senha'])
-        navegador.find_element('xpath', '/html/body/form/button/a').click()
-
-        #CAMPO 0
-        navegador.find_element('xpath', '/html/body/form/div[1]/label[1]/input').send_keys(Keys.CONTROL, 'v')
-        navegador.switch_to.window(navegador.window_handles[0])
-
-        #CAMPO 1
-        navegador.find_element('xpath', '//*[@id="resultado"]/div[1]/div[2]/button').click()
-        navegador.switch_to.window(navegador.window_handles[1])
-        navegador.find_element('xpath', '/html/body/form/div[1]/label[2]/input').send_keys(Keys.CONTROL, 'v')
-        navegador.switch_to.window(navegador.window_handles[0])
-
-        #CAMPO 2
-        navegador.find_element('xpath', '//*[@id="resultado"]/div[1]/div[3]/button').click()
-        navegador.switch_to.window(navegador.window_handles[1])
-        navegador.find_element('xpath', '/html/body/form/div[2]/label[1]/input').send_keys(Keys.CONTROL, 'v')
-        navegador.switch_to.window(navegador.window_handles[0])
-
-        #CAMPO 3
-        navegador.find_element('xpath', '//*[@id="resultado"]/div[2]/div[1]/button').click()
-        navegador.switch_to.window(navegador.window_handles[1])
-        navegador.find_element('xpath', '/html/body/form/div[2]/label[2]/input').send_keys(Keys.CONTROL, 'v')
-        navegador.switch_to.window(navegador.window_handles[0])
-
-        #CAMPO 4
-        navegador.find_element('xpath', '//*[@id="resultado"]/div[2]/div[1]/button').click()
-        navegador.switch_to.window(navegador.window_handles[1])
-        navegador.find_element('xpath', '/html/body/form/div[2]/label[2]/input').send_keys(Keys.CONTROL, 'v')
-        navegador.switch_to.window(navegador.window_handles[0])
-
-        #CAMPO 5
-        navegador.find_element('xpath', '/html/body/div/div[2]/div/section/div[2]/div[2]/button').click()
-        navegador.switch_to.window(navegador.window_handles[1])
-        navegador.find_element('xpath', '/html/body/form/div[3]/label[1]/input').send_keys(Keys.CONTROL, 'v')
-        navegador.switch_to.window(navegador.window_handles[0])
-
-        #CAMPO 6
-        navegador.find_element('xpath', '//*[@id="resultado"]/div[2]/div[3]/button').click()
-        navegador.switch_to.window(navegador.window_handles[1])
-        navegador.find_element('xpath', '/html/body/form/div[3]/label[2]/input').send_keys(Keys.CONTROL, 'v')
-        navegador.switch_to.window(navegador.window_handles[0])
-
-        #CAMPO 7
-        navegador.find_element('xpath', '//*[@id="resultado"]/div[3]/div[2]/button').click()
-        navegador.switch_to.window(navegador.window_handles[1])
-        navegador.find_element('xpath', '/html/body/form/div[4]/label[1]/input').send_keys(Keys.CONTROL, 'v')
-        navegador.switch_to.window(navegador.window_handles[0])
-
-        #CAMPO 8
-        navegador.find_element('xpath', '//*[@id="resultado"]/div[3]/div[3]/button').click()
-        navegador.switch_to.window(navegador.window_handles[1])
-        navegador.find_element('xpath', '/html/body/form/div[4]/label[2]/input').send_keys(Keys.CONTROL, 'v')
-        navegador.switch_to.window(navegador.window_handles[0])
-
-        #CAMPO 9
-        navegador.find_element('xpath', '//*[@id="resultado"]/div[4]/div[2]/button').click()
-        navegador.switch_to.window(navegador.window_handles[1])
-        navegador.find_element('xpath', '/html/body/form/div[5]/label[1]/input').send_keys(Keys.CONTROL, 'v')
-        navegador.switch_to.window(navegador.window_handles[0])
-
-        #CAMPO 10
-        navegador.find_element('xpath', '//*[@id="resultado"]/div[4]/div[2]/button').click()
-        navegador.switch_to.window(navegador.window_handles[1])
-        navegador.find_element('xpath', '/html/body/form/div[5]/label[2]/input').send_keys(Keys.CONTROL, 'v')
-        navegador.switch_to.window(navegador.window_handles[0])
-
-        #CAMPO 11
-        navegador.find_element('xpath', '//*[@id="resultado"]/div[4]/div[3]/button').click()
-        navegador.switch_to.window(navegador.window_handles[1])
-        navegador.find_element('xpath', '/html/body/form/label[1]/input').send_keys(Keys.CONTROL, 'v')
-        navegador.switch_to.window(navegador.window_handles[0])
-
-        #CAMPO 12
-        navegador.find_element('xpath', '//*[@id="resultado"]/div[4]/div[4]/button').click()
-        navegador.switch_to.window(navegador.window_handles[1])
-        navegador.find_element('xpath', '/html/body/form/label[2]/input').send_keys(Keys.CONTROL, 'v')
-        navegador.switch_to.window(navegador.window_handles[0])
-
-        #CAMPO 13
-        navegador.find_element('xpath', '//*[@id="resultado"]/div[4]/div[5]/button').click()
-        navegador.switch_to.window(navegador.window_handles[1])
-        navegador.find_element('xpath', '/html/body/form/label[3]/input').send_keys(Keys.CONTROL, 'v')
-        navegador.switch_to.window(navegador.window_handles[0])
+        navegador.get('*') # NO LUGAR DO * INSIRA A URL DO SITE ONDE SERA LIQUIDADO A NOTA FISCAL
